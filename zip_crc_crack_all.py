b4bd6c57b8a40c02bf5d9a6df91a9cd39b0d6157
playGitboy <72111157+playGitboy@users.noreply.github.com>
2020-10-26 06:55:54 +0800

    Add files via upload

---
+++ zip_crc_crack_all.py
@@ -0,0 +1,46 @@
+# 自动读取爆破加密zip所有1-4字节文件CRC，输出文件名(密码段顺序)和结果
+# 如果个别文件爆破失败，修改multiCrack中dic字典变量即可
+from zipfile import ZipFile
+import itertools, binascii
+
+def permutations_with_replacement(szDic, k):
+    result = []
+    for i in itertools.product(szDic, repeat = k):
+        result.append(''.join(i))
+    return result
+
+def multiCrack(iSize,lCrcs):
+    dic = '''abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!-@_{}'''
+    # dic = '''abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ '''
+    dResult = {}
+    mydic = permutations_with_replacement(dic,iSize)
+    for i in mydic:
+        txt = "".join(i)
+        crc = binascii.crc32(txt.encode())
+        if str(crc) in lCrcs:
+            print(hex(crc)+" "+txt)
+            dResult[str(crc)] = txt
+    return dResult
+
+def crack(lFilesCRC):
+    lResult = []
+    iSizes = set([int(i.split('|')[1]) for i in lFilesCRC])
+    for iSize in iSizes:
+        lCrcs = [i.split('|')[2] for i in lFilesCRC if i.split('|')[1]==str(iSize)]
+        dTMP = multiCrack(iSize,lCrcs)
+        for k,v in dTMP.items():
+            for i in lFilesCRC:
+                if i.find(k)>0:
+                    lResult.append(i+" "+v)
+    return lResult
+
+if __name__ == "__main__":
+    myzip = ZipFile(r"zip_crc.zip")
+    fInfos = myzip.filelist
+    lFilesCRC = []
+    for i in range(len(fInfos)):
+        fInfo = fInfos[i]
+        if fInfo.file_size < 5:
+            # 格式“文件名|原始大小|CRC”——如“1.txt|4|3463500836”
+            lFilesCRC.append(fInfo.filename + '|' + str(fInfo.file_size) + '|' + str(fInfo.CRC))
+    print(sorted(crack(lFilesCRC)))
